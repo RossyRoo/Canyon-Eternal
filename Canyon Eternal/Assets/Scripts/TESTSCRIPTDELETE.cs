2f9842888e6e2829a0fdbbb46b4c9f4cbae01fb6
RossyRoo <rossesmusic@gmail.com>
2021-07-03 09:20:56 -0400

    test changes from laptop

---
+++ Canyon Eternal/Assets/Scripts/TESTSCRIPTDELETE.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTSCRIPTDELETE : MonoBehaviour
+{
+    private void Awake()
+    {
+        //Hi
+    }
+}
