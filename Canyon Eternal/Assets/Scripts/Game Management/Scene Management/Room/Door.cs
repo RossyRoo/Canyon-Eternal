e35bb76954481fe2d6ed46684934cc9ef1c859aa
RossyRoo <rossesmusic@gmail.com>
2021-07-22 17:58:58 -0400

    Doors can be locked with displayed messages

--- Canyon Eternal/Assets/Scripts/Game Management/Scene Management/Room/Door.cs
+++ Canyon Eternal/Assets/Scripts/Game Management/Scene Management/Room/Door.cs
@@ -8,41 +8,74 @@ public class Door : Interactable
     public bool leadsToPreviousRoom;
     public Room otherRoom;
     public int doorNum = 0;
+    [Tooltip("If there is no lock, leave as null.")]
+    public string lockString;
+    bool doorIsLocked = false;
+    public GameObject lockedDoorMessageGO;
 
     public override void Interact(PlayerManager playerManager, PlayerStats playerStats)
     {
         base.Interact(playerManager, playerStats);
-        TransitionScenes(playerManager, playerStats);
+        TransitionScenes(playerManager, playerStats, playerStats.GetComponent<PlayerInventory>());
     }
 
-    private void TransitionScenes(PlayerManager playerManager, PlayerStats playerStats)
+    private void TransitionScenes(PlayerManager playerManager, PlayerStats playerStats, PlayerInventory playerInventory)
     {
-        SceneChangeManager sceneChangeManager = playerManager.GetComponentInParent<SceneChangeManager>();
+        CheckForLock(playerInventory);
 
-        if (leadsToNextRoom)
+        if(!doorIsLocked)
         {
-            playerManager.nextDoorNum = doorNum;
+            SceneChangeManager sceneChangeManager = playerManager.GetComponentInParent<SceneChangeManager>();
 
-            StartCoroutine(SceneChangeManager.Instance.ChangeScene(sceneChangeManager.roomList[sceneChangeManager.currentBuildIndex + 1].sceneNum));
-        }
-        else if(leadsToPreviousRoom)
-        {
-            playerManager.nextDoorNum = doorNum;
-
-            StartCoroutine(SceneChangeManager.Instance.ChangeScene(sceneChangeManager.roomList[sceneChangeManager.currentBuildIndex - 1].sceneNum));
-        }
-        else
-        {
-            if(otherRoom != null)
+            if (leadsToNextRoom)
             {
                 playerManager.nextDoorNum = doorNum;
 
-                StartCoroutine(SceneChangeManager.Instance.ChangeScene(otherRoom.sceneNum));
+                StartCoroutine(SceneChangeManager.Instance.ChangeScene(sceneChangeManager.roomList[sceneChangeManager.currentBuildIndex + 1].sceneNum));
             }
+            else if (leadsToPreviousRoom)
+            {
+                playerManager.nextDoorNum = doorNum;
+
+                StartCoroutine(SceneChangeManager.Instance.ChangeScene(sceneChangeManager.roomList[sceneChangeManager.currentBuildIndex - 1].sceneNum));
+            }
+            else
+            {
+                if (otherRoom != null)
+                {
+                    playerManager.nextDoorNum = doorNum;
+
+                    StartCoroutine(SceneChangeManager.Instance.ChangeScene(otherRoom.sceneNum));
+                }
+            }
+
+            playerStats.EnableInvulnerability(1.5f);
+        }
+        else
+        {
+            DisplayLockedDoorFX();
         }
 
+    }
 
-        playerStats.EnableInvulnerability(1.5f);
+    private void CheckForLock(PlayerInventory playerInventory)
+    {
+        if(lockString != null)
+        {
+            doorIsLocked = true;
 
+            for (int i = 0; i < playerInventory.itemInventory.Count; i++)
+            {
+                if(playerInventory.itemInventory[i].name == lockString)
+                {
+                    doorIsLocked = false;
+                }
+            }
+        }
+    }
+
+    private void DisplayLockedDoorFX()
+    {
+        Instantiate(lockedDoorMessageGO, transform.position, Quaternion.identity);
     }
 }
