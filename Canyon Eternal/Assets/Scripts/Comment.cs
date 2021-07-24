bd9a7ba178aa8d15c45051fc85c46b7c3ea46932
RossyRoo <rossesmusic@gmail.com>
2021-07-23 22:53:50 -0400

    Added room comments

---
+++ Canyon Eternal/Assets/Scripts/Comment.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comment : MonoBehaviour
+{
+    public string subject;
+
+    [TextArea]
+    public string comment;
+}
