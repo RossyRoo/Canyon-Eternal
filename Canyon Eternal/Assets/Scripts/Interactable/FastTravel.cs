1d8148693ebc16d8dfbe552b95f51ed6b77ca394
RossyRoo <rossesmusic@gmail.com>
2021-08-11 13:04:23 -0400

    added fast travel door art and created prefab - started script

---
+++ Canyon Eternal/Assets/Scripts/Interactable/FastTravel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastTravel : Interactable
+{
+    public override void Interact(PlayerManager playerManager, PlayerStats playerStats)
+    {
+        base.Interact(playerManager, playerStats);
+        OpenFastTravelMenu();
+    }
+
+    public void OpenFastTravelMenu()
+    {
+        Debug.Log("Opening fast travel menu");
+    }
+}
+ 
